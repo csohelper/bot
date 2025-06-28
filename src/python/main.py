03f4076c551e46ad9ac42933e4903a6b9e15c1f7
Slava Petrov <slavapmk@gmail.com>
2025-06-28 15:09:35 +0300

    Laundress command

--- src/python/main.py
+++ src/python/main.py
@@ -90,10 +90,10 @@ async def command_hr_handler(message: Message) -> None:
     await message.reply(get_string('echo_commands.hr'))
 
 
-@dp.message(Command("polyclinic"))
-@dp.message(lambda message: message.text and message.text.lower() in ["поликлиника"])
-async def command_polyclinic_handler(message: Message) -> None:
-    await message.reply(get_string('echo_commands.polyclinic'))
+@dp.message(Command("soft"))
+@dp.message(lambda message: message.text and message.text.lower() in ["софт", "программы", "программное обеспечение", "ПО"])
+async def command_soft_handler(message: Message) -> None:
+    await message.reply(get_string('echo_commands.soft'))
 
 
 @dp.message(Command("library"))
@@ -173,12 +173,51 @@ async def command_kitchen_handler(message: Message) -> None:
     )
 
 
+# @dp.message(Command("laundress"))
+# @dp.message(lambda message: message.text and message.text.lower() in ["прачка", "прачечная"])
+# async def command_laundress_handler(message: Message) -> None:
+#     await message.reply(
+#         get_string('echo_commands.laundress')
+#     )
+
+
+cached_laundress_files_id = []
+
 @dp.message(Command("laundress"))
-@dp.message(lambda message: message.text and message.text.lower() in ["прачка", "прачечная"])
+@dp.message(lambda message: message.text and message.text.lower() in ["прачка", "прачечная", "прачка биля"])
 async def command_laundress_handler(message: Message) -> None:
-    await message.reply(
-        get_string('echo_commands.laundress')
-    )
+    """
+    Отправляет пользователю изображения прачки из src/res/images/laundress.
+    Если изображения уже были отправлены ранее, то использует кэшированные file_id.
+    """
+
+    global cached_laundress_files_id
+
+    if len(cached_laundress_files_id) == 0:
+        laundress_dir = "./src/res/images/laundress/"
+        media=[
+            InputMediaPhoto(
+                media=FSInputFile(os.path.join(laundress_dir, x)),
+                show_caption_above_media=True
+            ) for x in os.listdir(laundress_dir)
+        ]
+        media[0].caption = get_string('echo_commands.laundress')
+        sent = await message.reply_media_group(media=media) # type: ignore[arg-type]
+        
+        for msg in sent:
+            if msg.photo:
+                largest_photo = msg.photo[-1]
+                cached_laundress_files_id.append(largest_photo.file_id)
+    else:
+        media = [
+            InputMediaPhoto(
+                media=file_id,
+                show_caption_above_media=True
+            ) for file_id in cached_laundress_files_id
+        ]
+        media[0].caption = get_string('echo_commands.laundress')
+        await message.reply_media_group(media=media) # type: ignore[arg-type]
+
 
 
 @dp.message(Command("washing"))
