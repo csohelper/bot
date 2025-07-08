526ee768ade04b1395363eb3b92abe0ae3a467f1
Slava Petrov <slavapmk@gmail.com>
2025-07-08 19:24:18 +0300

    Separated command handlers

---
+++ src/python/handlers/images_echo_commands.py
@@ -0,0 +1,166 @@
+import asyncio
+import os
+from aiogram import Router
+from ..strings import get_string
+from aiogram.types import Message
+from aiogram.filters import Command
+from ..logger import logger
+from aiogram.types import  FSInputFile, InputMediaPhoto, Message
+
+
+router = Router()
+
+
+cached_vost_file_id = None
+
+@router.message(Command("vost"))
+@router.message(lambda message: message.text and message.text.lower() in ["восточка"])
+async def command_vost_handler(message: Message) -> None:
+    await asyncio.sleep(1)
+    global cached_vost_file_id
+
+    while True:
+        if cached_vost_file_id is None:
+            image_path = "./src/res/images/cafe_vost.jpg"
+            sent: Message = await message.reply_photo(
+                photo=FSInputFile(image_path),
+                caption=get_string('echo_commands.cafe_vost'),
+                show_caption_above_media=True
+            )
+            if sent.photo:
+                cached_vost_file_id = sent.photo[-1].file_id
+        else:
+            try:
+                await message.reply_photo(
+                    photo=cached_vost_file_id,
+                    caption=get_string('echo_commands.cafe_vost'),
+                    show_caption_above_media=True
+                )
+            except Exception as e:
+                logger.error(f"{e}")
+                cached_vost_file_id = None
+                continue
+        break
+
+cached_linen_file_id = None
+
+@router.message(Command("linen"))
+@router.message(lambda message: message.text and message.text.lower() in ["обмен белья"])
+async def command_linen_handler(message: Message) -> None:
+    global cached_linen_file_id
+
+    while True:
+        if cached_linen_file_id is None:
+            image_path = "./src/res/images/linen.jpg"
+            sent: Message = await message.reply_photo(
+                photo=FSInputFile(image_path),
+                caption=get_string('echo_commands.linen'),
+                show_caption_above_media=True
+            )
+            if sent.photo:
+                cached_linen_file_id = sent.photo[-1].file_id
+        else:
+            try:
+                await message.reply_photo(
+                    photo=cached_linen_file_id,
+                    caption=get_string('echo_commands.linen'),
+                    show_caption_above_media=True
+                )
+            except Exception as e:
+                logger.error(f"{e}")
+                cached_linen_file_id = None
+                continue
+        break
+
+
+
+
+cached_laundress_files_id = []
+
+@router.message(Command("laundress"))
+@router.message(lambda message: message.text and message.text.lower() in ["прачка", "прачечная", "прачка биля"])
+async def command_laundress_handler(message: Message) -> None:
+    """
+    Отправляет пользователю изображения прачки из src/res/images/laundress.
+    Если изображения уже были отправлены ранее, то использует кэшированные file_id.
+    """
+
+    await asyncio.sleep(1)
+    global cached_laundress_files_id
+
+    while True:
+        if len(cached_laundress_files_id) == 0:
+            laundress_dir = "./src/res/images/laundress/"
+            media=[
+                InputMediaPhoto(
+                    media=FSInputFile(os.path.join(laundress_dir, x)),
+                    show_caption_above_media=True
+                ) for x in os.listdir(laundress_dir)
+            ]
+            media[0].caption = get_string('echo_commands.laundress')
+            sent = await message.reply_media_group(media=media) # type: ignore[arg-type]
+            
+            for msg in sent:
+                if msg.photo:
+                    largest_photo = msg.photo[-1]
+                    cached_laundress_files_id.append(largest_photo.file_id)
+        else:
+            try:
+                media = [
+                    InputMediaPhoto(
+                        media=file_id,
+                        show_caption_above_media=True
+                    ) for file_id in cached_laundress_files_id
+                ]
+                media[0].caption = get_string('echo_commands.laundress')
+                await message.reply_media_group(media=media) # type: ignore[arg-type]
+            except Exception as e:
+                logger.error(f"{e}")
+                cached_laundress_files_id = []
+                continue
+        break
+
+
+
+cached_cards_files_id = []
+
+@router.message(Command("cards"))
+@router.message(lambda message: message.text and message.text.lower() in ["карты"])
+async def command_cards_handler(message: Message) -> None:
+    """
+    Отправляет пользователю изображения карт из src/res/images/cards.
+    Если изображения уже были отправлены ранее, то использует кэшированные file_id.
+    """
+
+    await asyncio.sleep(1)
+    global cached_cards_files_id
+
+    while True:
+        if len(cached_cards_files_id) == 0:
+            cards_dir = "./src/res/images/cards/"
+            media=[
+                InputMediaPhoto(
+                    media=FSInputFile(os.path.join(cards_dir, x))
+                ) for x in os.listdir(cards_dir)
+            ]
+            media[-1].caption = get_string('echo_commands.cards')
+            sent = await message.reply_media_group(media=media) # type: ignore[arg-type]
+            
+            for msg in sent:
+                if msg.photo:
+                    largest_photo = msg.photo[-1]
+                    cached_cards_files_id.append(largest_photo.file_id)
+        else:
+            try:
+                media = [
+                    InputMediaPhoto(
+                        media=file_id
+                    ) for file_id in cached_cards_files_id
+                ]
+                media[-1].caption = get_string('echo_commands.cards')
+                await message.reply_media_group(media=media) # type: ignore[arg-type]
+            except Exception as e:
+                logger.error(f"{e}")
+                cached_cards_files_id = []
+                continue
+        break
---
+++ src/python/handlers/kek_command.py
@@ -0,0 +1,122 @@
+import asyncio
+import datetime
+import random
+from aiogram import Bot, Router
+from ..strings import get_string
+from aiogram.types import Message
+from aiogram.filters import Command
+from ..logger import logger
+from aiogram.enums import ChatMemberStatus
+from aiogram.exceptions import TelegramBadRequest, TelegramRetryAfter
+from aiogram.types import ChatPermissions, Message
+from ..config import config
+from .. import anecdote
+
+
+router = Router()
+bot: Bot
+
+kek_last_use = {}
+
+
+@router.message(Command("kek"))
+@router.message(lambda message: message.text and message.text.lower() in ["kek", "кек"])
+async def command_anecdote_handler(message: Message) -> None:
+    if message.chat.type not in ['group', 'supergroup']:
+        await message.reply(get_string(
+            'echo_commands.kek.only_group'
+        ))
+        return
+
+    global kek_last_use
+    if message.chat.id in kek_last_use:
+        last_use_chat = kek_last_use[message.chat.id]
+    else:
+        last_use_chat = datetime.datetime(2000, 1, 1, 0, 0)
+
+    if not message.from_user:
+        return
+
+    delta: datetime.timedelta = datetime.datetime.now() - last_use_chat
+    if delta < datetime.timedelta(seconds=30):
+        reply = await message.reply(get_string(
+            'echo_commands.kek.too_many', 
+            message.from_user.full_name,
+            30 - int(delta.total_seconds())
+        ))
+        await asyncio.sleep(5)
+        try:
+            await message.delete()
+        except Exception as e:
+            logger.error(f"Failed delete user message {message}: {e}")
+        try:
+            await reply.delete()
+        except Exception as e:
+            logger.error(f"Failed delete reply message {reply}: {e}")
+        return
+
+    if random.random() < 0.05:
+        me = await bot.get_me()
+        bot_member = await bot.get_chat_member(chat_id=message.chat.id, user_id=me.id)
+
+        if bot_member.status == ChatMemberStatus.ADMINISTRATOR and bot_member.can_restrict_members:
+            ban_time = random.randint(1, 30)
+            reply = await message.reply(get_string(
+                'echo_commands.kek.ban',
+                message.from_user.full_name,
+                ban_time
+            ))
+            user_member = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+            if user_member.status == ChatMemberStatus.ADMINISTRATOR:
+                await reply.edit_text(get_string(
+                    'echo_commands.kek.ban_admin',
+                    message.from_user.full_name
+                ))
+            else:
+                try:
+                    await bot.restrict_chat_member(
+                        chat_id=message.chat.id,
+                        user_id=message.from_user.id,
+                        permissions=ChatPermissions(can_send_messages=False),
+                        until_date=datetime.datetime.now() + datetime.timedelta(minutes=ban_time)
+                    )
+                except TelegramBadRequest as e:
+                    await reply.edit_text(get_string(
+                        'echo_commands.kek.ban_error',
+                        message.from_user.full_name,
+                        ban_time
+                    ))
+                    logger.error(f"Failed to restrict user {message.from_user.id} in chat {message.chat.id}. User not admin and bot has rights\n{e}")
+        else:
+            await message.reply(get_string(
+                'echo_commands.kek.ban_no_rights',
+                message.from_user.full_name
+            ))
+        return
+
+    kek_last_use[message.chat.id] = datetime.datetime.now()
+
+    if config.anecdote.enabled:
+        for i in range(100):
+            if i % 5 == 0:
+                try:
+                    await bot.send_chat_action(
+                        chat_id=message.chat.id,
+                        action='typing',
+                        message_thread_id=message.message_thread_id
+                    )
+                except TelegramRetryAfter:
+                    logger.warning("Telegram action type status restricted by flood control")
+            try:
+                text = await anecdote.get_anecdote()
+                if text:
+                    await message.reply(get_string(
+                        'echo_commands.kek.anecdote',
+                        text
+                    ))
+                    return                
+            except Exception as e:
+                logger.error(f"Failed to generate anecdote with exc {e}. Retrying...")
+    await message.reply(get_string(
+        'echo_commands.kek.not_found'
+    ))
