327ab89ac1a9156dd8ce90d5fb79f8d936fa275b
Slava Petrov <slavapmk@gmail.com>
2025-09-11 20:41:20 +0300

    Append

--- src/python/handlers/join_service.py
+++ src/python/handlers/join_service.py
@@ -1,11 +1,10 @@
 from aiogram import Router
 from aiogram.types import ChatJoinRequest
 
-from python.main import dp
 
 router = Router()
 
 
-@dp.chat_join_request()
+@router.chat_join_request()
 async def join_request(update: ChatJoinRequest):
     pass
