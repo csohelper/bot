5629ce8efbdd98bb837786f67aa534a38f12fc09
Slava Petrov <slavapmk@gmail.com>
2025-09-11 20:38:31 +0300

    Removed generated code

--- src/python/handlers/join_service.py
+++ src/python/handlers/join_service.py
@@ -1,24 +1,11 @@
 from aiogram import Router
 from aiogram.types import ChatJoinRequest
 
+from python.main import dp
+
 router = Router()
 
-# Обработчик события "подача заявки"
-@dp.chat_join_request_handler()
+
+@dp.chat_join_request()
 async def join_request(update: ChatJoinRequest):
-    user = update.from_user
-
-    # пример логирования
-    print(f"Заявка от {user.full_name} (@{user.username})")
-
-    # можно сразу одобрить
-    await update.approve()
-
-    # или отклонить:
-    # await update.decline()
-
-    # или отправить пользователю сообщение в личку
-    try:
-        await bot.send_message(user.id, "Спасибо за заявку! Админ рассмотрит её в ближайшее время.")
-    except:
-        pass
+    pass
