5efa15db45a539b8082655253063692c5802e6ae
Slava Petrov <slavapmk@gmail.com>
2025-09-11 20:45:27 +0300

    Removed unused table

--- src/python/storage/users_repository.py
+++ src/python/storage/users_repository.py
@@ -2,16 +2,17 @@ from python.storage import database
 
 
 async def init_database_module() -> None:
-    async with database.get_db_connection() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("""
-                CREATE TABLE IF NOT EXISTS users (
-                    id SERIAL PRIMARY KEY,
-                    user_id INTEGER NOT NULL,
-                    room INTEGER NULLABLE,
-                    username TEXT NOT NULL,
-                    fullname TEXT NOT NULL,
-                    status TEXT DEFAULT 'waiting'
-                )
-            """)
-            await conn.commit()
+    pass
+    # async with database.get_db_connection() as conn:
+    #     async with conn.cursor() as cur:
+    #         await cur.execute("""
+    #             CREATE TABLE IF NOT EXISTS users (
+    #                 id SERIAL PRIMARY KEY,
+    #                 user_id INTEGER NOT NULL,
+    #                 room INTEGER,
+    #                 username TEXT NOT NULL,
+    #                 fullname TEXT NOT NULL,
+    #                 status TEXT DEFAULT 'waiting'
+    #             )
+    #         """)
+    #         await conn.commit()
