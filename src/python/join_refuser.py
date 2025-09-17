43912ce412cb0fbc2ce8c9ac64fd1976a68e399e
Slava Petrov <slavapmk@gmail.com>
2025-09-18 01:00:00 +0300

    Debug logs

--- src/python/join_refuser.py
+++ src/python/join_refuser.py
@@ -3,6 +3,7 @@ import asyncio
 from aiogram import Bot
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
+from python.logger import logger
 from python.storage.config import config
 from python.storage.repository import users_repository
 from python.storage.strings import get_string
@@ -17,10 +18,14 @@ async def init(bot: Bot):
 
 
 async def refuser_loop_check() -> None:
+    logger.debug("Refuser loop check")
     requests = await users_repository.pop_unprocessed_requests_older_than(
         config.refuser.request_life_hours
     )
+    if len(requests) > 0:
+        logger.debug(f"Refuser: {requests} old requests")
     for request in requests:
+        logger.debug(f"Refuser: {request.user_id} {request.created_at} refused")
         await _bot.send_message(
             request.user_id,
             get_string(
