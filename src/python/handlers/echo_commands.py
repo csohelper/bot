2693cc4f0d1cc022261ddd82bbc0b4a68a984f92
Slava Petrov <slavapmk@gmail.com>
2025-09-17 23:17:15 +0300

    New handlers for deps

--- src/python/handlers/echo_commands.py
+++ src/python/handlers/echo_commands.py
@@ -35,7 +35,6 @@ commands = [
         ["жко", "жк", "жилищно коммунальный", "жилищно коммунальный отдел", "жилищно-коммунальный отдел"],
         'echo_commands.jko'
     ),
-    EchoCommand("ed", ["ед", "единый деканат", "деканат"], 'echo_commands.ed'),
     EchoCommand("hr", ["отдел кадров"], 'echo_commands.hr'),
     EchoCommand("soft", ["софт", "программы", "программное обеспечение", "ПО"], 'echo_commands.soft'),
     EchoCommand("library", ["библиотека"], 'echo_commands.library'),
@@ -43,11 +42,32 @@ commands = [
     EchoCommand("kitchen", ["кухня"], 'echo_commands.kitchen'),
     EchoCommand("polyclinic", ["поликлиника"], 'echo_commands.polyclinic'),
     EchoCommand("accounting", ["бухгалтерия"], 'echo_commands.accounting'),
+
+    EchoCommand("dean", ["деканат"], 'echo_commands.deanery.default'),
+    EchoCommand("ed", ["ед", "единый деканат"], 'echo_commands.deanery.ed'),
     EchoCommand("deanit", ["деканат ит"], 'echo_commands.deanery.it'),
     EchoCommand("deanrit", ["деканат рит"], 'echo_commands.deanery.rit'),
     EchoCommand("deannacs", ["деканат сисс"], 'echo_commands.deanery.nacs'),
     EchoCommand("deancais", ["деканат кииб"], 'echo_commands.deanery.cais'),
     EchoCommand("deandeamc", ["деканат цэимк"], 'echo_commands.deanery.deamc'),
+
+    EchoCommand("depritres", ["кафедра ртс"], 'echo_commands.departments.rit.res'),
+    EchoCommand("depritreac", ["кафедра рос"], 'echo_commands.departments.rit.reac'),
+    EchoCommand("depritelect", ["кафедра электроники"], 'echo_commands.departments.rit.electronics'),
+    EchoCommand("deprittasb", ["кафедра тизв"], 'echo_commands.departments.rit.tasb'),
+    EchoCommand("depritratsan", ["кафедра сисрт"], 'echo_commands.departments.rit.ratsan'),
+    EchoCommand("deprittec", ["кафедра тэц"], 'echo_commands.departments.rit.tec'),
+    EchoCommand("depritphys", ["кафедра физики"], 'echo_commands.departments.rit.physics'),
+    EchoCommand("depritteaa", ["кафедра тэдиа"], 'echo_commands.departments.rit.teaa'),
+
+    EchoCommand("depitmcait", ["кафедра мкиит"], 'echo_commands.departments.it.mcait'),
+    EchoCommand("depitmcis", ["кафедра кис"], 'echo_commands.departments.it.cis'),
+    EchoCommand("depitnitas", ["кафедра ситис"], 'echo_commands.departments.it.nitas'),
+    EchoCommand("depitma", ["кафедра матанализ"], 'echo_commands.departments.it.ma'),
+    EchoCommand("depitinf", ["кафедра информатика"], 'echo_commands.departments.it.informatics'),
+    EchoCommand("depitpe", ["кафедра физвосп"], 'echo_commands.departments.it.pe'),
+    EchoCommand("depitaai", ["кафедра пии"], 'echo_commands.departments.it.aai'),
+    EchoCommand("depitsp", ["кафедра сп"], 'echo_commands.departments.it.sp')
 ]
 
 
